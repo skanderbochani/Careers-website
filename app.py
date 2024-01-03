8ebd5488115abce4298af5c7dfb235b389d6634c
skanderbochani <77589797+skanderbochani@users.noreply.github.com>
2024-01-03 14:24:10 +0000

    added Bootstrap and Json file for the jobs

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
+JOBS = [
+  {
+    'id' : 1,
+    'title' : 'Data Analyst',
+    'location': 'Tunis, Tunisie',
+    'salary': '4000 TND'
+  },
+  {
+    'id' : 2,
+    'title' : 'software engineer',
+    'location': 'Benzart, Tunisie',
+    'salary': '4500 TND'
+  },
+  {
+    'id' : 3,
+    'title' : 'Data Scientist',
+    'location': 'Sfax, Tunisie',
+    'salary': '4000 TND'
+  },
+{
+  'id' : 4,
+  'title' : 'Cloud Engineer',
+  'location': 'Tunis, Tunisie',
+  'salary': '3000 TND'
+},
+]
 @app.route("/")
 def hello_MyCareers():
-  return render_template("home.html")
-
+  return render_template('home.html', jobs = JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug = True)
